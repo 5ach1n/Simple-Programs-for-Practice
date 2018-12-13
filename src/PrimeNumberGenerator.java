05885fbb2aa6460beb171c5f05bd88dc3f8b5779
Sachin <sachinbk0411@gmail.com>
2018-12-13 23:01:25 +0530

    Format code

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -1,25 +1,25 @@
 public class PrimeNumberGenerator {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Prime numbers from 1 to 100");
 
-        for(int i=2;i<=100;i++){
-            if(isPrime(i)){
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
                 System.out.println(i);
             }
         }
     }
 
-    public static boolean isPrime(int num){
-        if(num == 2 || num == 3){
+    public static boolean isPrime(int num) {
+        if (num == 2 || num == 3) {
             return true;
         }
 
-        if(num%2 == 0 || num%3 ==0){
+        if (num % 2 == 0 || num % 3 == 0) {
             return false;
         }
 
-        for(int i=3;i<Math.sqrt(num);i+=2){
-            if(num%i == 0 || num%Math.sqrt(num)==0){
+        for (int i = 3; i < Math.sqrt(num); i += 2) {
+            if (num % i == 0 || num % Math.sqrt(num) == 0) {
                 return false;
             }
         }
